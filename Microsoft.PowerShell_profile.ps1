76b8652a40b99382e2da70f425359988486ba7e0
Chase J <9084598+TheChaseJ@users.noreply.github.com>
2019-06-08 14:24:39 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,15 @@
-<#
-#####################################################################################
-Created      :  June 08, 2019
-Updated      :  June 08, 2019
-Filename     :  Microsoft.PowerShell_profile.ps1
-Autor        :  TheChaseJ (https://github.com/TheChaseJ)
-Description  :  Personal PowerShell profile v1.0
-Repository   :  https://github.com/TheChaseJ/Microsoft.PowerShell_profile.ps1
-#####################################################################################
+
+<#	
+	.NOTES
+	===========================================================================================================
+  Created      :  June 08, 2019
+  Updated      :  June 08, 2019
+  Filename     :  Microsoft.PowerShell_profile.ps1
+  Autor        :  TheChaseJ (https://github.com/TheChaseJ)
+  Repository   :  https://github.com/TheChaseJ/PowerShell-Profile/Microsoft.PowerShell_profile.ps1
+	===========================================================================================================
+  .DESCRIPTION
+	Personal PowerShell profile v1.0.
 #>
 
 set-location C:\
